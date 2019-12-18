aefbc742ffffb121c829fac47fb0dd8b28a2e117
Oscar <oscarcgm@hotmail.com>
2019-12-18 17:45:06 +0800

    try fetch

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -5,21 +5,51 @@ import { Button } from 'antd';
 class Welcome extends React.Component {
     constructor (props){
         super(props)
+
+        this.state = {
+            list: []
+        }
         this.handleClick = this.handleClick.bind(this)
 
     }
 
     handleClick (){
-        console.log(1)
+        // console.log(1)
+        fetch('https://10.10.10.234:8080/token?page=0&size=20')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({list:data})
+        })
+        .catch(err => alert(err))
     }
 
     render (){
         return (
             <div>
-                <Button type="primary" onClick={this.handleClick}>Primary</Button>
                 <span>欢迎</span>
+                <Button type="primary" onClick={this.handleClick}>Primary</Button>
+                <List list={this.list}></List>
+
             </div>
         )
+    }
+}
+
+class List extends React.Component {
+    constructor (props){
+        super (props)
+    }
+
+    render (){
+        return (
+            <ul>
+            {
+    (this.props.list||[]).map((item, index) => <li key={index}>{item.name}</li>)
+            }
+        </ul>
+        )
+        
     }
 }
 
