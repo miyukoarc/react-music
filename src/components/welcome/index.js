3407ff6fe5cc3c8b6c57d35861da8c7097d2ec8c
Oscar <oscarcgm@hotmail.com>
2019-12-18 09:45:59 +0800

    start

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 // import { Row, Col } from 'antd';
+import { Button } from 'antd';
 
 class Welcome extends React.Component {
+    constructor (props){
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+
+    }
+
+    handleClick (){
+        console.log(1)
+    }
+
     render (){
         return (
             <div>
+                <Button type="primary" onClick={this.handleClick}>Primary</Button>
                 <span>欢迎</span>
             </div>
         )
